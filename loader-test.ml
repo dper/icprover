ee3cb1de39f8dd87f50a75a67b031300383fd60c
Douglas Perkins <webmaster@dperkins.org>
2015-10-31 22:23:22 +0900

    Removing redundant license notices.

--- loader-test.ml
+++ loader-test.ml
@@ -1,22 +1,3 @@
-(*
- * Copyright 2007, 2008, Douglas Perkins.
- * 
- * This file is part of ICProver.
- * 
- * ICProver is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- * 
- * ICProver is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License
- * along with ICProver.  If not, see <http://www.gnu.org/licenses/>.
- *)
-
 (* Returns true iff all questions in the file are proven.
 	- The file must exist and be readable.
 	@param file The file name.
