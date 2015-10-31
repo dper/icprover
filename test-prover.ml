10d5caab21eda83e18584450f1392ee20973b90d
Douglas Perkins <webmaster@dperkins.org>
2015-11-01 01:22:52 +0900

    Messing about with parsing.

--- test-prover.ml
+++ test-prover.ml
@@ -3,18 +3,29 @@
 #use "prover.ml";;
 #load "str.cma";;
 
-(* Parses a string of a question.
-   The format should look like (A & B) |- G. *)
+(*
+	Parses a string of a question.
+	The format should look like (A & B) |- G.
+
+	Useful string functions documented here:
+	http://caml.inria.fr/pub/docs/manual-ocaml/libref/Str.html
+*)
 module Parser = struct
+	let parseFormula (s:string):Formula.formula =
+		let s = String.trim s in
+		print_endline s ;
+		Formula.Bottom	
 
-	let splitQuestion (s:string) =
-		Str.split (Str.regexp "|-") s
+	let parseContext (s:string):Formula.formula list =
+		let fs = Str.split (Str.regexp ",") s in
+		List.map parseFormula fs
 
-	let parse (s:string):Question.question =
-		let context = [] in
-		let goal = Formula.Bottom in
-			(context, goal)
-
+	let parseQuestion (s:string):Question.question =
+		match Str.split (Str.regexp "|-") s with
+		| c :: g :: [] -> (parseContext c, parseFormula g)
+		| _ -> failwith ("Invalid question: " ^ s)
 end
+;;
 
-print_endline "What?"
+Parser.parseQuestion "A, C |- B";;
+print_endline "Printing.";;
