3d7f4f629749db7cf694fd8bca6110ebb7a6c53e
Douglas Perkins <webmaster@dperkins.org>
2015-11-01 12:24:29 +0900

    Working on parser.

--- test-prover.ml
+++ test-prover.ml
@@ -4,18 +4,27 @@
 #load "str.cma";;
 
 (*
-	Parses a string of a question.
-	The format should look like (A & B) |- G.
+	Parses a string of a question.  For example, (A & B), C |- D.
 
 	Useful string functions documented here:
 	http://caml.inria.fr/pub/docs/manual-ocaml/libref/Str.html
 *)
 module Parser = struct
+	(*
+		Parses a string and returns a formula.
+		Invariants: The string must be a well-formed formula.
+		@param s The string to be parsed.
+	*)
 	let parseFormula (s:string):Formula.formula =
 		let s = String.trim s in
 		print_endline s ;
 		Formula.Bottom	
 
+	(*
+		Parses a context string and returns the formulae.
+		Invariants: The string must be comma-separated formula strings.
+		@param s The string to be parsed.
+	*)
 	let parseContext (s:string):Formula.formula list =
 		let fs = Str.split (Str.regexp ",") s in
 		List.map parseFormula fs
@@ -24,9 +33,7 @@ module Parser = struct
 		Parses a string and returns a formula for it.
 		Internally, we switch to single-character operators.
 		White space is ignored.  
-
 		Invariants: The string must be a well-formed formula.
-
 		@param s The string to be parsed.
 	*)
 	let parseQuestion (s:string):Question.question =
